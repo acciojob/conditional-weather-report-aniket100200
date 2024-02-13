e14f5d43c02d9b9eee2d13433917c5365b314fad
Aniket Khangar <116418804+aniket100200@users.noreply.github.com>
2024-02-13 16:32:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,40 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import './../styles/App.css';
+let state=[
+    {
+        temprature:25,
+        conditions: "Sunny"
+    },
+    {
+        temprature:25,
+        conditions: "Cloudy"
+    },
+    {
+        temprature:15,
+        conditions: "Cold"
+    },
+    {
+        temprature:15,
+        conditions: "Rainy"
+    },
+    {
+        temprature:40,
+        conditions: "Hot"
+    },
+]
 
+const App = () => {
+  return (
+    <div>
+        {
+            state.map(item=><>
+                    {/* I'll use shock circuit.. */}
+                  {item.temprature>20 ? <p style={{color:"red"}}>{item.temprature} {item.conditions}</p>:<p style={{color:"blue"}}>{item.temprature} {item.conditions}</p>}
+            </>)
+        }
+      
+    </div>
+  )
+}
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+export default App
